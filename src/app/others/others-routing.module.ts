89658bf32c7f278f2317f8406fe1958f7ec8529f
yamano <yamanoryohey@gmail.com>
2020-12-30 11:13:06 +0900

    initial

---
+++ src/app/others/others-routing.module.ts
@@ -0,0 +1,108 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { OthersComponent } from './others.component';
+import { OthersMainComponent } from './others-main/others-main.component';
+import { ScheffeUraVersionComponent } from './scheffe-ura-version/scheffe-ura-version.component';
+import { Cluster3DModelComponent } from './cluster-3dmodel/cluster-3dmodel.component';
+import { CppTensorflowComponent } from './cpp-tensorflow/cpp-tensorflow.component';
+import { StartVTuberComponent } from './start-vtuber/start-vtuber.component';
+import { StartVTuber2Component } from './start-vtuber-2/start-vtuber-2.component';
+import { UnityNativePluginTipsComponent } from './unity-nativeplugin-tips/unity-nativeplugin-tips.component';
+import { UnityRealSenseComponent } from './unity-realsense/unity-realsense.component';
+import { ABCJSComponent } from './abcjs/abcjs.component';
+
+const routes: Routes = [
+    {
+        path: '', component: OthersComponent,
+        children: [
+            {
+                path: '', component: OthersMainComponent,
+                data: {
+                    title: 'その他 | 佐藤 優志 (Yushi Sato)',
+                    description: '佐藤優志のその他のページです。',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,Tips,Blog',
+                    ogUrl: 'https://youseegreen.github.io/others/'
+                }
+            },
+            {
+                path: 'scheffe-ura-version', component: ScheffeUraVersionComponent,
+                data: {
+                    title: 'シェッフェの一対比較法の浦の変法の元論文について | 佐藤 優志 (Yushi Sato)',
+                    description: 'シェッフェの一対比較法の浦の変法の元論文について。',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,シェッフェの一対比較法,浦の変法,参考文献',
+                    ogUrl: 'https://youseegreen.github.io/others/scheffe-ura-version'
+                }
+            },
+            {
+                path: 'cluster-3dmodel', component: Cluster3DModelComponent,
+                data: {
+                    title: 'BlenderとUnityでCluster用の.vrmファイルを作成する | 佐藤 優志 (Yushi Sato)',
+                    description: 'BlenderとUnityでCluster用の.vrmファイルを作成する',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,Blender, Unity, Cluster, vrm',
+                    ogUrl: 'https://youseegreen.github.io/others/cluster-3dmodel'
+                }
+            },
+            {
+                path: 'cpp-tensorflow', component: CppTensorflowComponent,
+                data: {
+                    title: 'c++から学習済みtensorflowモデルを呼び出す | 佐藤 優志 (Yushi Sato)',
+                    description: 'c++から学習済みtensorflowモデルを呼び出す',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato, c++, tensorflow, keras, visual studio',
+                    ogUrl: 'https://youseegreen.github.io/others/cpp-tensorflow'
+                }
+            },
+            {
+                path: 'start-vtuber', component: StartVTuberComponent,
+                data: {
+                    title: 'USBカメラを使ってV-Tuberになろう！1 | 佐藤 優志 (Yushi Sato)',
+                    description: 'USBカメラを使ってV-Tuberになろう！1',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,USBカメラ, V-Tuber, c++, Unity',
+                    ogUrl: 'https://youseegreen.github.io/others/start-vtuber'
+                }
+            },
+            {
+                path: 'start-vtuber-2', component: StartVTuber2Component,
+                data: {
+                    title: 'USBカメラを使ってV-Tuberになろう！２ | 佐藤 優志 (Yushi Sato)',
+                    description: 'USBカメラを使ってV-Tuberになろう！２',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,USBカメラ, V-Tuber, c++, Unity',
+                    ogUrl: 'https://youseegreen.github.io/others/start-vtuber-2'
+                }
+            },
+            {
+                path: 'unity-nativeplugin-tips', component: UnityNativePluginTipsComponent,
+                data: {
+                    title: 'UnityのNetivePlugin利用でc++上でのDebugを少し便利にしてみる | 佐藤 優志 (Yushi Sato)',
+                    description: 'UnityのNetivePlugin利用でc++上でのDebugを少し便利にしてみる',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,c++, dll, Unity, Native Plugins, Debug, 開発',
+                    ogUrl: 'https://youseegreen.github.io/others/unity-nativeplugin-tips'
+                }
+            },
+            {
+                path: 'unity-realsense', component: UnityRealSenseComponent,
+                data: {
+                    title: 'Native Pluginを利用してUnityからRealsenseを利用する | 佐藤 優志 (Yushi Sato)',
+                    description: 'Native Pluginを利用してUnityからRealsenseを利用する',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,Unity, RealSense, Native Plugins, c++, dll',
+                    ogUrl: 'https://youseegreen.github.io/others/unity-realsense'
+                }
+            },
+            {
+                path: 'abcjs', component: ABCJSComponent,
+                data: {
+                    title: 'abcjsを用いて、ウェブページで楽譜を表示してみる | 佐藤 優志 (Yushi Sato)',
+                    description: 'abcjsを用いて、ウェブページで楽譜を表示してみる',
+                    keyword: '佐藤優志,youseegreen,Yushi Sato,Unity, RealSense, Native Plugins, c++, dll',
+                    ogUrl: 'https://youseegreen.github.io/others/abcjs'
+                }
+            },
+
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class OthersRoutingModule { }
---
+++ src/app/others/scheffe-ura-version/scheffe-ura-version.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scheffe-ura-version',
+  templateUrl: './scheffe-ura-version.component.html',
+  styleUrls: ['../others.component.css']
+})
+export class ScheffeUraVersionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
